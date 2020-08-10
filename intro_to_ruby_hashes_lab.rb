035998728ee94d1e19a133abe03009d449f01041
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 21:01:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,24 +8,24 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {:railroad=> {
-    :pieces => 4,
-    :rent_in_dollars => {},
-    :names => {} } 
+    :pieces=> 4,
+    :rent_in_dollars=> {},
+    :names=> {} } 
   }
 end
 
 def monopoly_with_third_tier
   {:railroad=>
   {
-    :pieces => 4,
-    :rent_in_dollars =>
+    :pieces=> 4,
+    :rent_in_dollars=>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-    :names =>
+    :names=>
     {
       :reading_railroad=>{},
       :pennsylvania_railroad=>{},
@@ -39,15 +39,15 @@ end
 def monopoly_with_fourth_tier
   {:railroad=>
   {
-    :pieces => 4,
-    :rent_in_dollars =>
+    :pieces=> 4,
+    :rent_in_dollars=>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-    :names =>
+    :names=>
     {
       :reading_railroad=>{ :mortgage_value=>100},
       :pennsylvania_railroad=>{ :mortgage_value=>200},
