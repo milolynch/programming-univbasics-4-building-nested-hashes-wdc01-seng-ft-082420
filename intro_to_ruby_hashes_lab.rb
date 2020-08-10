5507c68f63008093bbe707aaf1994c0b82a21168
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 20:28:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,15 +42,15 @@ def monopoly_with_fourth_tier
   # complexity.
   {:railroad => 
   {:pieces : 4, :rent_in_dollars : {
-      :one_piece_owned : 25
-      :two_piece_owned : 50
-      :three_piece_owned : 100
-      :four_piece_owned : 200
+      :one_piece_owned => 25
+      :two_piece_owned => 50
+      :three_piece_owned => 100
+      :four_piece_owned => 200
   }, :names : {
-      :reading_railroad : { :mortgage_value : 100 }
-      :pennsylvania_railroad : { :mortgage_value : 200 }
-      :b_and_o_railroad : { :mortgage_value : 300 }
-      :shortline_railroad : { :mortgage_value : 400 }
+      :reading_railroad : { :mortgage_value => 100 }
+      :pennsylvania_railroad : { :mortgage_value => 200 }
+      :b_and_o_railroad : { :mortgage_value => 300 }
+      :shortline_railroad : { :mortgage_value => 400 }
   } 
     
   } 
