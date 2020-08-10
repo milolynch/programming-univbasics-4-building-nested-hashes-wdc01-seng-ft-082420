84c2f5fb9f329f1130b90f62f9211ba6429192b8
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 20:25:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,10 @@ def monopoly_with_third_tier
       :three_piece_owned : 100
       :four_piece_owned : 200
     }, :names : {
-      :reading_railroad
-      :pennsylvania_railroad
-      :b_and_o_railroad
+      :reading_railroad : {}
+      :pennsylvania_railroad : {}
+      :b_and_o_railroad : {}
+      :shortline_railroad : {}
     } 
     
     } 
@@ -39,5 +40,20 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  
+    {:railroad => 
+    {:pieces : 4, :rent_in_dollars : {
+      :one_piece_owned : 25
+      :two_piece_owned : 50
+      :three_piece_owned : 100
+      :four_piece_owned : 200
+    }, :names : {
+      :reading_railroad : { :mortgage : 100}
+      :pennsylvania_railroad : {}
+      :b_and_o_railroad : {}
+      :shortline_railroad : {}
+    } 
+    
+    } 
+    
+  }
 end
