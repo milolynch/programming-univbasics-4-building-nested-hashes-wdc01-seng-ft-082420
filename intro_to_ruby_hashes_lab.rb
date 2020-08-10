c9906497c4462b5d7f35cf8c121e08ae8ecbec8f
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 20:52:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {:railroad => {
+  {:railroad =>
+  {
     :pieces => 4,
     :rent_in_dollars =>
     {
@@ -26,10 +27,31 @@ def monopoly_with_third_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-    :names => {} } 
+    :names => {
+      
+    } 
+  } 
   }
 end
 
 def monopoly_with_fourth_tier
-  
+  {:railroad =>
+  {
+    :pieces => 4,
+    :rent_in_dollars =>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names =>
+    {
+      #
+      #
+      #
+    } 
+    
+  } 
+  }
 end
